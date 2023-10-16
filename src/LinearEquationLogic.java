7caad0339bb60f7afee9ce544dd8518b70397ebe
BT_2N2_03 <sample@bths.edu>
2023-10-16 14:01:18 -0400

    initial commit

--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class LinearEquationLogic {
+import java.util.Scanner;
+public class LinearEquationLogic {
+
+
+    public void start() {
+
+    }
+
+    public void welcome() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to the linear equation calculator!");
+        System.out.println("Enter coordinate pair 1: ");
+        String coordPair1 = scan.nextLine();
+        System.out.println("Enter coordinate pair 2: ");
+        String coordPair2 = scan.nextLine();
+    }
 }
